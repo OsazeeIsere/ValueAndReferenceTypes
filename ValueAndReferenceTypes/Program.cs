147021985cc2cb8b0f5ae1ad1bf1844a5b9314b2
Osazee Isere <oz1.concepts@gmail.com>
2021-06-12 18:30:59 +0100

    working with valueAndReference

--- ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace ValueAndReferenceTypes
 {
+    struct Point
+    {
+
+        // fields of the structure
+        public int x;
+        public int y;
+
+        // add 1 to the (x,y) position
+        public void Increment()
+        {
+            x++; y++;
+        }
+
+        //subtract 1 from x and y position
+        public void Decrement()
+        {
+            x--; y--;
+        }
+
+        //display the current position
+        public void Display()
+        {
+            Console.WriteLine("X={0}, Y={1}", x, y);
+            Console.WriteLine();
+        }
+
+        //a custom constructor
+        public void CustomConstructor(int xPos, int yPos)
+        {
+            x = xPos;
+            y = yPos;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+        }
+        //Assigning two intrinsic value type results in 
+        //two independent variables on the stack.
+        static void ValueTypeAssignment()
+        {
+
         }
     }
 }
